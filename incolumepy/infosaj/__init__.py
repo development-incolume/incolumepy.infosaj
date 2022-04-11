1a1f0c33f2369ba50f1dda2919489670dfe5a6b4
Britodfbr <britodfbr@gmail.com>
2022-04-11 06:59:34 -0300

    chore: Unificação de número de versão

--- incolumepy/infosaj/__init__.py
+++ incolumepy/infosaj/__init__.py
@@ -1,2 +1,15 @@
-"""Principal module."""
-__version__ = "0.1.0"
+"""
+Principal Module.
+
+Update metadata from version by semver
+"""
+from pathlib import Path
+
+import toml
+
+configfile = Path(__file__).parents[2].joinpath("pyproject.toml")
+versionfile = Path(__file__).parent.joinpath("version.txt")
+versionfile.write_text(
+    f"{toml.load(configfile)['tool']['poetry']['version']}\n"
+)
+__version__ = versionfile.read_text().strip()
