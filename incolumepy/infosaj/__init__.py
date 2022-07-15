73fdf90ddfbc6f61bd824f69723cc86e29c9fca7
Britodfbr <britodfbr@gmail.com>
2022-07-14 21:02:47 -0300

    refactor: CLI incorporada ao projeto principal

--- incolumepy/infosaj/__init__.py
+++ incolumepy/infosaj/__init__.py
@@ -4,12 +4,14 @@ Principal Module.
 Update metadata from version by semver
 """
 from pathlib import Path
-
-import toml
+try:
+    import tomllib
+except ModuleNotFoundError:
+    import tomli as tomllib
 
 configfile = Path(__file__).parents[2].joinpath("pyproject.toml")
 versionfile = Path(__file__).parent.joinpath("version.txt")
 versionfile.write_text(
-    f"{toml.load(configfile)['tool']['poetry']['version']}\n"
+    f"{tomllib.loads(configfile.read_text())['tool']['poetry']['version']}\n"
 )
 __version__ = versionfile.read_text().strip()
